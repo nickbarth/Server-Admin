64befb3446ad5f8536e774a351d0c7b62976f054
Nick Barth <nickbarth@live.com>
2015-09-10 10:54:07 -0600

    Update ftp_setup.sh

--- ftp_setup.sh
+++ ftp_setup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-apt-get install vsftpd
+# Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+apt-get install -y vsftpd
+sed -i "s/#\(chroot_local_user=YES\)/\1/" /etc/vsftpd.conf
+sed -i "s/#\(write_enable=YES\)/\1/" /etc/vsftpd.conf
+
+sudo service vsftpd restart
