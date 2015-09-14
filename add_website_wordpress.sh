0d3cfb82aa77b9c800da28f0ef6cc27f60c36a0d
Nick Barth <nickbarth@live.com>
2015-09-14 13:24:15 -0600

    Update add_website_wordpress.sh

--- add_website_wordpress.sh
+++ add_website_wordpress.sh
@@ -25,10 +25,16 @@ curl -s https://wordpress.org/latest.tar.gz | tar -zxf - --directory /var/www/$D
 cd /var/www/$DOMAIN
 mv wp-config-sample.php wp-config.php
 
+# Database
 sed -i "s/'DB_NAME', 'database_name_here'/'DB_NAME', '$DOMAIN'/" wp-config.php
 sed -i "s/'DB_USER', 'username_here'/'DB_USER', '$DOMAIN'/" wp-config.php
 sed -i "s/'DB_HOST', 'localhost'/'DB_HOST', '$PASSWORD'/" wp-config.php
 sed -i "s/'DB_PASSWORD', 'password_here'/'DB_PASSWORD', '$PASSWORD'/" wp-config.php
 
-
-
+# Salts
+curl -s https://api.wordpress.org/secret-key/1.1/salt/ | while read LINE; do
+  FIND=$(echo $LINE | grep -o "'[^']*',")
+  LINENUMBER=$(grep -n "$FIND" ./wp-config.php | awk -F: '{ print $1 }')
+  sed -i "${LINENUMBER}d" ./wp-config.php
+  sed -i "${LINENUMBER}i${LINE}" ./wp-config.php
+done
