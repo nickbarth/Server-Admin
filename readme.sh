1cbd8d94401b4ac95caeb324d4bc0cb8ed966fa4
Nick Barth <nickbarth@live.com>
2015-09-14 10:24:55 -0600

    Update and rename make_readme.sh to readme.sh

--- readme.sh
+++ readme.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+####
+# Generates a readme with explaination of scripts.
+#
+# USAGE: ./readme.sh.sh
+##
 
 echo "# Server-Admin" > readme.md
 echo "Linux Admin Scripts for Ubuntu Trusty 14.04 Amd64 Server 20150325 (ami-d05e75b8)" >> readme.md
