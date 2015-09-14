291043ef50ea32e46e2bc8954918d736ed02d11a
Nick Barth <nickbarth@live.com>
2015-09-14 10:51:56 -0600

    Update add_website_wordpress.sh

--- add_website_wordpress.sh
+++ add_website_wordpress.sh
@@ -6,6 +6,11 @@
 # USAGE: ./add_website.sh example.com
 ##
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 source ./add_website_user.sh
 
 curl -s https://wordpress.org/latest.tar.gz | tar -zxf - --directory $DOMAIN --strip-components=1 wordpress
