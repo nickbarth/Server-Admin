9fe92c826288241427860496c7b893e268907791
Nick Barth <nickbarth@live.com>
2015-09-14 11:44:10 -0600

    Update config_database.sh

--- config_database.sh
+++ config_database.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+####
+# Configure database connection settings.
+#
+# USAGE: source ./config_database.sh
+#
+# EXPORTS: MYSQL_HOST, MYSQL_ADMIN, MYSQL_PASSWORD
+# eg. main.example.us-east-1.rds.amazonaws.com, root, passwd
+##
+
 MYSQL_HOST=""
 MYSQL_ADMIN=""
 MYSQL_PASSWORD=""
