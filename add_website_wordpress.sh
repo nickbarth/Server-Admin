abd57fba578e3d09d6e1b7fdc9742ba5cea167e9
Nick Barth <nickbarth@live.com>
2015-09-14 09:22:55 -0600

    Update add_website_wordpress.sh

--- add_website_wordpress.sh
+++ add_website_wordpress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DIRECTORY=$1
-curl -s https://wordpress.org/latest.tar.gz | tar -zxf - --directory $DIRECTORY --strip-components=1 wordpress
+DOMAIN=$1
+curl -s https://wordpress.org/latest.tar.gz | tar -zxf - --directory $DOMAIN --strip-components=1 wordpress
