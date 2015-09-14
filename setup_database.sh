bca758924747b511a91687971115a831290dd988
Nick Barth <nickbarth@live.com>
2015-09-14 09:42:49 -0600

    Update setup_database.sh

--- setup_database.sh
+++ setup_database.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+MYSQL_ROOT_PASSWD=$1
+
+# Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 mysql --host="localhost" --user="root" --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWD');"
