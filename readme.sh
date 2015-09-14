1423464b1c1f73ac41965d481e68787f8e3855fb
Nick Barth <nickbarth@live.com>
2015-09-14 10:41:38 -0600

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -6,13 +6,19 @@
 # USAGE: ./readme.sh
 ##
 
-echo "# Server-Admin" > readme.md
-echo "Linux Admin Scripts for Ubuntu Trusty 14.04 Amd64 Server 20150325 (ami-d05e75b8)" >> readme.md
+echo '# Server-Admin' > readme.md
+echo >> readme.md
+echo 'Linux Admin Scripts for Ubuntu Trusty 14.04 Amd64 Server 20150325 (ami-d05e75b8)' >> readme.md
+echo >> readme.md
 
+echo '```bash' >> readme.md
 for SCRIPT in $(ls *.sh); do
   echo $SCRIPT >> readme.md
   awk '/^####$/,/^##$/' $SCRIPT >> readme.md
   echo >> readme.md
 done
+echo '```' >> readme.md
 
-echo "MIT &copy; 2015 Nick Barth" >> readme.md
+echo >> readme.md
+echo 'MIT &copy; 2015 Nick Barth' >> readme.md
+cat readme.md
