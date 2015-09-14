e382981df6e3d00da604859dd973dc0e65d4376a
Nick Barth <nickbarth@live.com>
2015-09-14 10:51:34 -0600

    Update add_website_wordpress.sh

--- add_website_wordpress.sh
+++ add_website_wordpress.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+####
+# Add, configure, and setup a Wordpress installation. 
+#
+# USAGE: ./add_website.sh example.com
+##
+
 source ./add_website_user.sh
 
 curl -s https://wordpress.org/latest.tar.gz | tar -zxf - --directory $DOMAIN --strip-components=1 wordpress
