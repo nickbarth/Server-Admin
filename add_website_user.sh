96ed2b6cc5177b52e703cbcc3eb9c509452963d4
Nick Barth <nickbarth@live.com>
2015-09-14 11:58:42 -0600

    Update add_website_user.sh

--- add_website_user.sh
+++ add_website_user.sh
@@ -3,7 +3,7 @@
 ####
 # Create a website user and give them a folder with FTP access.
 #
-# USAGE: ./add_website_user.sh example_com
+# USAGE: ./add_website_user.sh example_com passwd
 ##
 
 # Run as Root
@@ -14,7 +14,9 @@ fi
 
 # Domain lowercased and no periods eg. example_com
 DOMAIN=$(echo ${1,,} | sed 's/\./_/g')
+PASSWORD=$2
 
 adduser --system --ingroup www-data --home /var/www/$DOMAIN $DOMAIN
+echo "$DOMAIN:$PASSWORD" | chpasswd
 
 service vsftpd restart
