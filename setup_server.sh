848287f2a1e1aeed92c074fed04963ed86344428
Nick Barth <nickbarth@live.com>
2015-09-14 09:46:02 -0600

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -15,4 +15,4 @@ fi
 # Update and Install Dependencies
 apt-get -y update
 apt-get -y upgrade
-DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 mysql-server php5 mysql-client php5-common php5-mysql php5-xmlrpc php5-cgi php5-curl php5-gd php5-cli php5-fpm php-apc php-pear php5-dev php5-imap php5-mcrypt libapache2-mod-php5 libxml2 ca-certificates curl
+apt-get install -y apache2 mysql-client php5 php5-common php5-mysql php5-xmlrpc php5-cgi php5-curl php5-gd php5-cli php5-fpm php-apc php-pear php5-dev php5-imap php5-mcrypt libapache2-mod-php5 libxml2 ca-certificates curl
