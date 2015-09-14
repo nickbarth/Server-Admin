48dfaa861621fc7beb11948b19af359bc4071def
Nick Barth <nickbarth@live.com>
2015-09-14 09:51:43 -0600

    Update add_database.sh

--- add_database.sh
+++ add_database.sh
@@ -6,17 +6,17 @@
 # USAGE: ./create_database main.example.us-east-1.rds.amazonaws.com mysql_root _passwd example_com _expasswd
 ###
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 MYSQL_HOST=$1
 MYSQL_ADMIN=$2
 MYSQL_PASSWORD=$3
 
 DATABASE=$4
 PASSWORD=$5
-
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 1>&2
-  exit 1
-fi
 
 mysql --host="$MYSQL_HOST" --user="$MYSQL_ADMIN" --password="$MYSQL_PASSWORD" --execute="CREATE DATABASE $DATABASE;"
 mysql --host="$MYSQL_HOST" --user="$MYSQL_ADMIN" --password="$MYSQL_PASSWORD" --execute="CREATE USER '$DATABASE'@'%' IDENTIFIED BY '$PASSWORD';"
