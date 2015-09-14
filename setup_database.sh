df24afcb0731249b76fdbef39b401c38d530eff2
Nick Barth <nickbarth@live.com>
2015-09-14 09:56:05 -0600

    Update setup_database.sh

--- setup_database.sh
+++ setup_database.sh
@@ -3,16 +3,16 @@
 ####
 # Install and setup a MySQL Server.
 #
-# USAGE: ./setup_database.sh
+# USAGE: ./setup_database.sh "%MySQLRootPasswd%"
 ##
-
-MYSQL_ROOT_PASSWD=$1
 
 # Run as Root
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
+
+MYSQL_ROOT_PASSWD=$1
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 mysql --host="localhost" --user="root" --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWD');"
