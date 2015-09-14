177a81e5d7928c8438be083c89b92e9ce7e89ac6
Nick Barth <nickbarth@live.com>
2015-09-14 13:42:03 -0600

    Update bak_domain.sh

--- bak_domain.sh
+++ bak_domain.sh
@@ -11,3 +11,9 @@ if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root" 1>&2
   exit 1
 fi
+
+# Domain lowercased and no periods eg. example_com
+DOMAIN=$(echo ${1,,} | sed 's/\./_/g')
+
+./bak_website.sh
+./bak_database.sh
