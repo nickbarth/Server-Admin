3ba9b34a1e4e3d1ef7f14c3d38bec9ba53e94f0c
Nick Barth <nickbarth@live.com>
2015-09-14 11:50:54 -0600

    Update bak_database.sh

--- bak_database.sh
+++ bak_database.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-mysqldump -u $MYSQL_USER -p$MYSQL_PASSWD -h $MYSQL_HOST $MYSQL_DB > $MYSQL_DB-$(date +%Y-%m-%d).backup.sql
+####
+# Backup a database to an SQL file.
+#
+# USAGE: ./bak_database.sh example.com
+##
+
+# Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# Import: MYSQL_HOST, MYSQL_ADMIN, MYSQL_PASSWORD
+source ./database_config.sh
+
+# Domain lowercased and no periods eg. example_com
+DOMAIN=$(echo ${1,,} | sed 's/\./_/g')
+
+mysqldump -u $MYSQL_ADMIN -p$MYSQL_PASSWD -h $MYSQL_HOST $DOMAIN > $DOMAIN-$(date +%Y-%m-%d).backup.sql
