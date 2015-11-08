e104888411010f8912ade1825ede5b3eb763234c
Nick Barth <nickbarth@live.com>
2015-11-08 12:58:37 -0700

    Update add_website_from_curl.sh

--- add_website_from_curl.sh
+++ add_website_from_curl.sh
@@ -45,7 +45,7 @@ mysql --host="$MYSQL_HOST" --user="$MYSQL_ADMIN" --password="$MYSQL_PASSWORD" --
 mysql --host="$MYSQL_HOST" --user="$MYSQL_ADMIN" --password="$MYSQL_PASSWORD" --execute="GRANT ALL PRIVILEGES ON $DOMAIN.* TO '$DOMAIN'@'%';"
 mysql --host="$MYSQL_HOST" --user="$MYSQL_ADMIN" --password="$MYSQL_PASSWORD" --execute="FLUSH PRIVILEGES;"
 
-cat << EOF >> /etc/apache2/sites-available/$DOMAIN.conf
+cat << EOF > /etc/apache2/sites-available/$DOMAIN.conf
 <VirtualHost *:80>
   ServerName $RAWDOMAIN
   # ServerAlias *.$RAWDOMAIN
